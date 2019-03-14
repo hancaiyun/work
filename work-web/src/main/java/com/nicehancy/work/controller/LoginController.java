7b62fcf3226351a844cb2ee4233f0a5376afd61d
hancaiyun <hancaiyun-it@bestpay.com.cn>
2019-03-14 20:04:02 +0800

    1、web配置文件 2、版本升级脚本

---
+++ work-web/src/main/java/com/nicehancy/work/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.nicehancy.work.controller;
+
+/**
+ * 登陆管理controller
+ * <p>
+ *     1、登陆
+ *     2、注册
+ * <p/>
+ *
+ * @author hancaiyun
+ * @since 2019/3/14 14:50
+ **/
+public class LoginController {
+}
