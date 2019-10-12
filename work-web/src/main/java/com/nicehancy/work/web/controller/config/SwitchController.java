5d33c8e9ac568871fe3d7c10725b45be3af62996
hancaiyun <hancaiyun-it@bestpay.com.cn>
2019-10-12 16:31:22 +0800

    数据源切换测试

--- work-web/src/main/java/com/nicehancy/work/web/controller/config/SwitchController.java
+++ work-web/src/main/java/com/nicehancy/work/web/controller/config/SwitchController.java
@@ -1,11 +1,19 @@
 package com.nicehancy.work.web.controller.config;
 
+import com.nicehancy.work.service.api.model.base.Result;
+import com.nicehancy.work.service.config.DataSourceSwitchServiceImpl;
 import com.nicehancy.work.web.controller.base.BaseController;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  *  系统配置controller
@@ -20,6 +28,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/config")
 public class SwitchController extends BaseController {
 
+    @Autowired
+    private DataSourceSwitchServiceImpl dataSourceSwitchService;
+
+    /**
+     * 开关页面
+     * @return     页面视图
+     */
     @RequestMapping(value = "/switch")
     @ResponseBody
     public ModelAndView getPage(){
@@ -29,4 +44,22 @@ public class SwitchController extends BaseController {
         return mode;
     }
 
+    /**
+     * 数据源切换
+     * @param request   请求参数
+     * @return
+     */
+    @RequestMapping(value = "/switchCheck")
+    @ResponseBody
+    public ModelMap switchCheck(HttpServletRequest request){
+
+        String traceLogId = UUID.randomUUID().toString();
+        MDC.put("TRACE_LOG_ID", traceLogId);
+        String datasource = this.getParameters(request).get("datasource");
+        log.info("SwitchController switchCheck request PARAM: datasource={}, traceLogId={}", datasource, traceLogId);
+
+        Result<Boolean> result =  dataSourceSwitchService.switchDataSource(datasource, traceLogId);
+
+        return this.processSuccessJSON(result);
+    }
 }
